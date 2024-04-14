3aa02a1b98ecffd60f3ada224b2aadc9fef5d6e4
zucrose <uzrhmdkhn@gmail.com>
2024-04-14 15:33:05 +0530

    fixed bug

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { createServer } = require("node:http");
 const { Server } = require("socket.io");
 const cors = require("cors");
-import dotenv from "dotenv";
+const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
